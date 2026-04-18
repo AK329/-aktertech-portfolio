5aa9b5fbe1328cebe2a67c11d015e6936936bc71
AK329 <sagorhs329@gmail.com>
2026-04-18 20:30:21 +0600

    Update About text again

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,8 +6,8 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I am a high-achieving EEE undergraduate student at Varendra University
-          with a 3.88 CGPA. Despite being in my 1st year, I have already built a
+          I am an undergraduate student at Varendra University.
+          Despite being in my 1st year, I have already built a
           strong foundation in Python and Machine Learning. I am passionate about
           data science, predictive modeling, and building practical tools with
           Python. Currently seeking a Research Assistant position to deepen my
